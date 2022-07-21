eb79c75356b6bab9c050f8d34a0d2bbbb6a0e082
jovanVukomanovic <vukomanovicjovan@gmail.com>
2022-07-21 10:30:14 +0200

    react blog

---
+++ src/Create.js
@@ -0,0 +1,74 @@
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+    const [isPending, setIsPending] = useState(false);
+    const history = useNavigate();
+
+    console.log(history);
+   
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const blog = {title,body,author};
+         
+        setIsPending(true);
+        console.log(blog);
+
+        fetch('http://localhost:8000/blogs',{
+            method:'POST',
+            headers:{"Content-Type": "aplication/json"},
+            body:JSON.stringify(blog)
+        }).then(()=>{
+            console.log('new blog added')
+            setIsPending(false);
+            // history.go(-1);
+            history('/');
+
+        })
+
+        console.log(blog);
+    }
+
+    return ( 
+        <div className="create">
+            <h2>Add a New blog</h2>
+            <form onSubmit={handleSubmit} >
+                <label > Blog title: </label>
+                    <input type="text"
+                            required  
+                            value={title}
+                            onChange={(e)=> setTitle(e.target.value)}
+                            
+                            />
+                        
+                <label > Blog Body:</label>
+                    <textarea
+                    required
+                    value={body}
+                    onChange={(e)=>setBody(e.target.value)}
+                    ></textarea>
+                <label >Blog author:</label>
+                    <select
+                    value={author}
+                    onChange={(e)=>setAuthor(e.target.value)}
+                     >
+                        <option value="mario">mario</option>
+                        <option value="yoshi">yoshi</option>
+                    </select>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button>Adding a blog...</button>}
+
+               
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
+        </div>
+     );
+}
+ 
+export default Create;
